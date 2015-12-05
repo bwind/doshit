3ed85998fb8ba962a6541e0c0ed835ba28c88912
gregchlamers <g.chalmers@metocean.co.nz>
2015-12-05 15:28:17 +1300

    fixes

--- python-dothis/setup.py
+++ python-dothis/setup.py
@@ -1,24 +1,12 @@
 from distutils.core import setup
 from distutils.command.install import install
-from distutils import archive_util
-from glob import iglob
-from shutil import copy
-from os.path import join
-from os.path import basename
-
-def copy_files(src_glob, dst_folder):
-    for fname in iglob(src_glob):
-        copy(fname, join(dst_folder, basename(fname)))
 
 class custom_install(install):
     def run(self):
         install.run(self)
-        os.symlink()
-        ln -s /etc/sv/consul /etc/service
-        #archive_util.mkpath("/etc/cloudburst", mode=774)
-        #copy_files('cloudburst/config/*.yml', '/etc/cloudburst')
+        
 
-setup(name='dothis',
+print setup(name='dothis',
       version='0.0.1',
       description='DoThis',
       author='MetOcean Solutions',
@@ -28,6 +16,5 @@ setup(name='dothis',
 
       cmdclass={'install': custom_install},
 
-      install_requires=["redis",
-                        "psutil"],
+      requires=["redis", "psutil"],
       )

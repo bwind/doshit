ef3d87c7e9261d1db505fe729b367db168c73145
gregchlamers <g.chalmers@metocean.co.nz>
2015-12-05 14:32:55 +1300

    adding picture, removed some swear words

--- python-dothis/dothis/settings.py
+++ python-dothis/dothis/settings.py
@@ -1,6 +1,7 @@
 import os
 import json_serializer as json
 
-REDIS_CONNECTION = json.load(os.getenv('DOSHIT_REDIS',
+REDIS_CONNECTION = json.load(os.getenv('DOTHIS_REDIS',
                                        '{"host": "localhost", "port": 6379, "db": 0 }'))
-QUEUE_NAME = os.getenv('DOSHIT_QUEUE', None)
+
+QUEUE_NAME = os.getenv('DOTHIS_QUEUE', 'dothis')
